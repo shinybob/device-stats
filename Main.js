fc247964eaab5e548cbc307f4589a0db54bd0f83
rob.thurston <rob.thurston@gamesys.co.uk>
2019-03-13 18:39:33 +0000

    setup

--- Main.js
+++ Main.js
@@ -1,3 +1,4 @@
+const Server = require("./Server");
 
 module.exports = class Main {
 
@@ -6,6 +7,17 @@ module.exports = class Main {
     }
 
     setupDatabase() {
-        console.log('ploppy')
+        console.log('setupDatabase')
+        this.setupServer();
+    }
+
+    setupServer() {
+        console.log('setupServer')
+        this.server = new Server(this);
+        this.server.startServer(this.onServerCreated.bind(this));
+    }
+
+    onServerCreated(port) {
+        console.info("Server created on port:" + port);
     }
 };
