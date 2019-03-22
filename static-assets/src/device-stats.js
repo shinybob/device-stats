52e12f7c2180c205e755a1ae15ac11dd755937f5
robthurston <rob.thurston@gamesys.co.uk>
2019-03-22 11:35:49 +0000

    Update device-stats.js

--- static-assets/src/device-stats.js
+++ static-assets/src/device-stats.js
@@ -20,14 +20,14 @@ function init() {
     this.deviceInput.className = 'input';
     this.additionalDataInput.className = 'input';
     this.submitButton.className = 'button';
-    this.deviceInput.placeholder = 'Device Name';
-    this.additionalDataInput.placeholder = 'Additional Data';
+    // this.deviceInput.placeholder = 'Device Name';
+    // this.additionalDataInput.placeholder = 'Additional Data';
 
-    this.submitButton.addEventListener('click', this.onSubmitClicked.bind(this))
+    // this.submitButton.addEventListener('click', this.onSubmitClicked.bind(this))
 
-    this.deviceInput.id = 'deviceName';
-    this.additionalDataInput.id = 'additionalData';
-    this.submitButton.id = 'submitButton';
+    // this.deviceInput.id = 'deviceName';
+    // this.additionalDataInput.id = 'additionalData';
+    // this.submitButton.id = 'submitButton';
 
     header.innerText = "Device Stats";
 
@@ -39,100 +39,100 @@ function init() {
     container.appendChild(submitButton);
     container.appendChild(resultField);
 
-    window.addEventListener('resize', update);
+    // window.addEventListener('resize', update);
 
     document.body.appendChild(this.container);
 
-    canvas = document.createElement('canvas');
+    // canvas = document.createElement('canvas');
 
-    if (canvas) {
-        context = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
-        if (context) {
-            var info = context.getExtension('WEBGL_debug_renderer_info');
-            if (info) {
-                renderer = context.getParameter(info.UNMASKED_RENDERER_WEBGL);
-            }
-        }
-    }
+    // if (canvas) {
+    //     context = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
+    //     if (context) {
+    //         var info = context.getExtension('WEBGL_debug_renderer_info');
+    //         if (info) {
+    //             renderer = context.getParameter(info.UNMASKED_RENDERER_WEBGL);
+    //         }
+    //     }
+    // }
 
     this.logMessage('init 3');
 
-    update();
+    // update();
 }
 
-function onSubmitClicked() {
-    var deviceData = {
-        deviceName:this.deviceInput.value,
-        additionalData:this.additionalDataInput.value,
-        height:(screen.width >= screen.height) ? screen.width : screen.height,
-        width:(screen.width < screen.height) ? screen.width : screen.height,
-        renderer:getRenderer(),
-        maxAnisotropy:getMaxAnisotropy(),
-        devicePixelRatio:window.devicePixelRatio,
-    }
+// function onSubmitClicked() {
+//     var deviceData = {
+//         deviceName:this.deviceInput.value,
+//         additionalData:this.additionalDataInput.value,
+//         height:(screen.width >= screen.height) ? screen.width : screen.height,
+//         width:(screen.width < screen.height) ? screen.width : screen.height,
+//         renderer:getRenderer(),
+//         maxAnisotropy:getMaxAnisotropy(),
+//         devicePixelRatio:window.devicePixelRatio,
+//     }
 
-    var callback = this.onSubmitionComplete.bind(this);
-    var request = new XMLHttpRequest();
+//     var callback = this.onSubmitionComplete.bind(this);
+//     var request = new XMLHttpRequest();
 
-    request.open('POST', '/addDevice', true);
-    request.setRequestHeader('Content-type', 'application/json');
-    request.onreadystatechange = () => {
-        if (request.readyState === 4 && request.status === 200) {
-            callback(request);
-        }
-    };
+//     request.open('POST', '/addDevice', true);
+//     request.setRequestHeader('Content-type', 'application/json');
+//     request.onreadystatechange = () => {
+//         if (request.readyState === 4 && request.status === 200) {
+//             callback(request);
+//         }
+//     };
 
-    request.send(JSON.stringify(deviceData));
+//     request.send(JSON.stringify(deviceData));
 
-    console.log('onSubmitClicked');
-    console.log(deviceData);
+//     console.log('onSubmitClicked');
+//     console.log(deviceData);
 
-    this.container.innerHTML = "Thank you!<br><br><a href='" + window.location + "getDeviceList'>Click here to view device list.</a>"
-}
+//     this.container.innerHTML = "Thank you!<br><br><a href='" + window.location + "getDeviceList'>Click here to view device list.</a>"
+// }
 
 function logMessage(message) {
     var field = document.getElementById("log")
     field.innerText = message + "\n" + field.innerText;
 }
 
-function onSubmitionComplete(request) {
-    console.log(request);
-}
+// function onSubmitionComplete(request) {
+//     console.log(request);
+// }
 
-function update() {
-    this.logMessage('update 1');
-    var text = '';
+// function update() {
+//     this.logMessage('update 1');
+//     var text = '';
 
-    text += 'Screen size: ' + screen.width + ' x ' +  screen.height + '\n';
-    text += 'Window size: ' + window.innerWidth + ' x ' +  window.innerHeight + '\n';
-    text += 'Renderer: ' + getRenderer() + '\n';
-    text += 'Max Anisotropy: ' + getMaxAnisotropy() + '\n';
-    text += 'devicePixelRatio: ' + window.devicePixelRatio + '\n';
-    text += 'userAgent: ' + window.navigator.userAgent + '\n';
+//     text += 'Screen size: ' + screen.width + ' x ' +  screen.height + '\n';
+//     text += 'Window size: ' + window.innerWidth + ' x ' +  window.innerHeight + '\n';
+//     text += 'Renderer: ' + getRenderer() + '\n';
+//     text += 'Max Anisotropy: ' + getMaxAnisotropy() + '\n';
+//     text += 'devicePixelRatio: ' + window.devicePixelRatio + '\n';
+//     text += 'userAgent: ' + window.navigator.userAgent + '\n';
 
-    this.resultField.innerText = text;
+//     this.resultField.innerText = text;
 
-    this.logMessage('update 2');
-}
+//     this.logMessage('update 2');
+// }
 
-function getRenderer() {
-    return renderer || 'unknown';
-}
+// function getRenderer() {
+//     return renderer || 'unknown';
+// }
 
-function getMaxAnisotropy() {
-    var max = 0;
+// function getMaxAnisotropy() {
+//     var max = 0;
 
-    if (context) {
-        var extension = (
-            context.getExtension('EXT_texture_filter_anisotropic') ||
-            context.getExtension('MOZ_EXT_texture_filter_anisotropic') ||
-            context.getExtension('WEBKIT_EXT_texture_filter_anisotropic')
-        );
+//     if (context) {
+//         var extension = (
+//             context.getExtension('EXT_texture_filter_anisotropic') ||
+//             context.getExtension('MOZ_EXT_texture_filter_anisotropic') ||
+//             context.getExtension('WEBKIT_EXT_texture_filter_anisotropic')
+//         );
 
-        if (extension) {
-            max = context.getParameter(extension.MAX_TEXTURE_MAX_ANISOTROPY_EXT);
-        }
-    }
+//         if (extension) {
+//             max = context.getParameter(extension.MAX_TEXTURE_MAX_ANISOTROPY_EXT);
+//         }
+//     }
 
-    return max;
-}
+//     return max;
+// }
