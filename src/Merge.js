492518fe50f638080093c1595a35f13d6409deef
rob.thurston <rob.thurston@gamesys.co.uk>
2019-03-26 18:31:51 +0000

    Update Merge.js

--- src/Merge.js
+++ src/Merge.js
@@ -1,10 +1,17 @@
 const MongoClient = require('mongodb').MongoClient;
 
+/**
+ * Updates DB1 with values from DB2 based on testObjectName
+ */
+
+// Public
 const DB_1_URL = 'mongodb://shinybob:kennard@ds159509.mlab.com:59509/device-stats';
 const DB_1_NAME = 'device-stats';
 const DB_1_COLLECTION = 'deviceStats';
 
-const DB_2_URL = 'mongodb://10.225.179.18:27017/device-stats';
+// Private
+const DB_2_URL = 'mongodb://localhost:27017/device-stats';
+// const DB_2_URL = 'mongodb://10.225.179.18:27017/device-stats';
 const DB_2_NAME = 'device-stats';
 const DB_2_COLLECTION = 'devices';
 
@@ -16,41 +23,89 @@ async function init() {
     let db2 = await connect(DB_2_URL, DB_2_NAME, DB_2_COLLECTION);
 
     let list1 = await getDevices(db1);
-    let list2 = await getDevices(db2);
+
     let devicesToAdd = [];
     
     for (var value1 of list1) {
-        let found = false;
 
-        for (var value2 of list2) {
-            if(value1.deviceName === value2.testObjectName) {
-                found = true;
-            }
-        }
+        let make = getMake(value1.deviceName);
+        let model = formatModel(make, value1.deviceName);   
 
-        if(!found) {
-            devicesToAdd.push({
-                testObject:true,
-                testObjectName:value1.deviceName,
-                model:value1.deviceName,
-                screenWidth:value1.width,
-                screenHeight:value1.height,
-                pixelRatio:value1.devicePixelRatio,
-                userAgent:value1.userAgent,
-                renderer:value1.renderer,
-                maxAnisotropy:value1.maxAnisotropy,
-                active:false,
-                platform:'mobile',
-            });
-        }
+        devicesToAdd.push({
+            testObject:true,
+            testObjectName:value1.deviceName,
+            make:make,
+            model:model,
+            screenWidth:value1.width,
+            screenHeight:value1.height,
+            pixelRatio:value1.devicePixelRatio,
+            userAgent:value1.userAgent,
+            renderer:value1.renderer,
+            maxAnisotropy:value1.maxAnisotropy,
+            active:false,
+            platform:'mobile'
+        });
     }
 
     try {
-        db2.insertMany( devicesToAdd );
-        console.log ('done');
-     } catch (e) {
+        // db2.deleteMany( { testObject : true } ).then(() => {
+            // console.log ('done deleting');
+            devicesToAdd.forEach(element => {
+                db2.updateOne({testObjectName:element.testObjectName}, element, {upsert:true});
+                console.log ('adding: ' + element.testObjectName);
+            });
+        // })
+        // .catch(e => {
+            // console.log (e);
+        // });
+    } catch (e) {
         console.log (e);
-     }
+    }
+}
+
+function formatModel(make, deviceName) {
+    if(!isApple(make)) {
+        deviceName = deviceName.substr(deviceName.indexOf('_') + 1);
+    }
+
+    if(deviceName.indexOf('_real') > -1) {
+        deviceName = deviceName.substr(0, deviceName.indexOf('_real'));
+    };
+
+    if(deviceName.indexOf('_gsys') > -1) {
+        deviceName = deviceName.substr(0, deviceName.indexOf('_gsys'));
+    };
+
+    if(deviceName.indexOf('_free') > -1) {
+        deviceName = deviceName.substr(0, deviceName.indexOf('_free'));
+    };
+
+    deviceName = deviceName.split('_').join(' ');
+
+    return deviceName;
+}
+
+function getMake(deviceName) {
+    let make = deviceName.split('_')[0];
+
+    if(make.length <= 4) {
+        make = make.toUpperCase();
+    } else {
+        make = make.charAt(0).toUpperCase() + make.slice(1).toLowerCase();
+    }
+
+    if(isApple(make)) {
+        make = 'Apple';
+    }
+
+    return make
+}
+
+function isApple(make) {
+    return (
+        make.toLowerCase() === 'ipad' || 
+        make.toLowerCase() === 'iphone' ||
+        make.toLowerCase() === 'apple');
 }
 
 function getDevices(db) {
